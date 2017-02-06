1136657a45e578c9f56d981333e10126ca4e9464
Carsten Aulbert <carsten.aulbert@aei.mpg.de>
2017-02-06 21:42:26 +0100

    Start with simple mockup

--- generate.rb
+++ generate.rb
@@ -1,12 +1,23 @@
 #!/usr/bin/env ruby
 
+# this script will generate all necessary meta-packages (rpm and deb)
+# based on the main config file and what is already present under the
+# target directory
+
 require 'yaml'
 
+# where to find meta information about packages
+meta_dir = 'meta'
+
+# where to stage packages - will create this one and RPM/DEB dirs underneath
+stage_dir = 'stage'
+
 # load default file
-packages = YAML.load_file('mockup.yaml')
+packages = YAML.load_file("#{meta_dir}/all.yml")
 
 require 'pp'
 pp packages.inspect
+
 
 packages.keys.sort.each do |pkg|
   packages[pkg].keys.sort.each do |d|
